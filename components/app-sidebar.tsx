41b3d3b42ff16b311e81a79583864a9ec4e591c5
Tanay Appannagari <tapp2004@berkeley.edu>
2024-11-15 17:54:01 +0700

    landing page edits

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -35,8 +35,12 @@ const nav = [
         url: "/#flow-chart",
       },
       {
-        title: "Conclusion",
+        title: "Purpose",
         url: "/#conclusion",
+      },
+      {
+        title: "Personal Experience and Feedback",
+        url: "/#personal-experience",
       },
     ],
   },
