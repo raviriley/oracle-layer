5e2e2ce2b8eb43a3fc7b7fc12e455098e515b96d
Ravi Riley <raviriley@gmail.com>
2024-11-15 19:36:20 +0700

    Add CORS allow all

--- avs/py-server/main.py
+++ avs/py-server/main.py
@@ -7,6 +7,14 @@ import re
 app = Flask(__name__)
 
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
 @app.route("/create_oracle", methods=["POST"])
 def create_oracle_route():
     # Parse request body as JSON
